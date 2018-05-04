4d80da62a643552d6047b51126c6e48bb1ce3ec7
Anthony Foulfoin <anthony.foulfoin@carboatmedia.fr>
2018-05-04 17:54:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+Trakt.tv duplicate / additionnal plays bulk removal  
+===========  
+This script allows you to remove all the duplicate or additionnal plays for your trakt.tv movies or episodes, and just keep one play per movie / episode (the oldest one).
+
+For instance if you have 3 plays for the movie **Guardians of the Galaxy** at these dates:
+
+ - 2014-08-01T20:00:00.000Z
+ - 2017-05-05T22:00:00.000Z
+ - 2018-02-05T21:00:00.000Z
+
+The script will keep the oldest play (2014-08-01T20:00:00.000Z) and remove all the others.
+If there are no duplicates the script do nothing.
+The script backup your data in local json files (movies.json and episodes.json) before doing anything.
+  
+## Getting Started  
+### Trakt.tv  
+  
+Go [register a new api app]( https://trakt.tv/oauth/applications/new). And fill the form with theses informations:  
+  
+```  
+Name: trakt-duplicates-removal  
+Redirect uri: urn:ietf:wg:oauth:2.0:oob  
+```  
+  
+You don't really care about the others fields.  
+  
+Next, go to [your api applications](https://trakt.tv/oauth/applications) and click on the one named `trakt-duplicates-removal`  
+  
+You will need the `Client ID` and the `Client Secret` from that page.  
+  
+### The script  
+You will need python3 and pip installed and in your path
+ 
+ 1. Start by doing a `pip install` at the root of the project 
+ 2. Edit the script to set the `client_id` and `client_secret` variables with the ones you obtained on your trakt.tv api application
+ 3. Edit the script to set the `username`variable with your trakt.tv username (you can find it in your settings: https://trakt.tv/settings)
+ 4. Optionally you can modify the `types` variable to run the script only for **movies** or **episodes**
+ 5. Launch the script :  
+```  
+python3 trakt-duplicates-removal.py  
+```  
+Follow the script instructions
+
+### Credits 
+
+Based on https://gist.github.com/blaulan/50d462696bce5da9225b8d596a8c6fb4
---
+++ trakt-duplicates-removal.py
@@ -0,0 +1,121 @@
+import json
+import requests
+
+# Edit the informations bellow
+client_id = 'YOUR CLIENT ID'
+client_secret = 'YOUR CLIENT SECRET'
+username = 'YOUR USERNAME'
+
+# Optional
+types = ['movies', 'episodes']  # 'movies' or 'episodes' or both
+
+
+# Don't edit the informations bellow
+trakt_api = 'https://api.trakt.tv'
+auth_get_token_url = '%s/oauth/token' % trakt_api
+get_history_url = '%s/users/%s/history/{type}?page={page}&limit={limit}' % (trakt_api, username)
+sync_history_url = '%s/sync/history/remove' % trakt_api
+
+session = requests.Session()
+
+
+def login_to_trakt():
+    print('Authentication')
+    print('Open the link in a browser and paste the pin')
+    print('https://trakt.tv/oauth/authorize?response_type=code&client_id=%s&redirect_uri=urn:ietf:wg:oauth:2.0:oob' % client_id)
+    print('')
+
+    pin = str(input('Pin: '))
+
+    session.headers.update({
+        'Accept': 'application/json',
+        'User-Agent': 'Betaseries to Trakt',
+        'Connection': 'Keep-Alive'
+    })
+
+    post_data = {
+        'code': pin,
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob',
+        'grant_type': 'authorization_code'
+    }
+
+    request = session.post(auth_get_token_url, data=post_data)
+    response = request.json()
+
+    print(response)
+    print()
+
+    session.headers.update({
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': client_id,
+        'Authorization': 'Bearer ' + response["access_token"]
+    })
+
+
+def get_history(type):
+    results = []
+
+    url_params = {
+        'page': 1,
+        'limit': 1000,
+        'type': type
+    }
+
+    print('Retrieving history for %s' % type)
+
+    while True:
+        print(get_history_url.format(**url_params))
+        resp = session.get(get_history_url.format(**url_params))
+
+        if resp.status_code != 200:
+            print(resp)
+            continue
+
+        results += resp.json()
+
+        if int(resp.headers['X-Pagination-Page-Count']) != url_params['page']:
+            url_params['page'] += 1
+        else:
+            break
+
+    print('Done retrieving %s history' % type)
+    return results
+
+
+def remove_duplicate(history, type):
+    print('Removing %s duplicates' % type)
+
+    entry_type = 'movie' if type == 'movies' else 'episode'
+
+    entries = []
+    duplicates = []
+
+    for i in history[::-1]:
+        if i[entry_type]['ids']['trakt'] in entries:
+            duplicates.append(i['id'])
+        else:
+            entries.append(i[entry_type]['ids']['trakt'])
+
+    if len(duplicates) > 0:
+        print('%s %s duplicates plays to be removed' % (len(duplicates), type))
+
+        session.post(sync_history_url, json={'ids': duplicates})
+        print('%s %s duplicates successfully removed!' % (len(duplicates), type))
+    else:
+        print('No %s duplicates found' % type)
+
+
+if __name__ == '__main__':
+    login_to_trakt()
+
+    for type in types:
+        history = get_history(type)
+        with open('%s.json' % type, 'w') as output:
+            json.dump(history, output, indent=4)
+            print('History saved in file %s.json' % type)
+
+        remove_duplicate(history, type)
+        print()
